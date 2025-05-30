d9cee9e3044bc3293a3468e171843139da3dc561
김진현 <wlsgus63@naver.com>
2025-05-30 14:37:05 +0900

    Feat: 변수 연습

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi"); //Add code
+
+        int age = 25;
+
+        System.out.println(age);
+
+        age+=1;
+
+        System.out.println(age);
+
+        byte b = 18;
+
+        double salary = 50000.75;
+
+        System.out.println(salary);
+
+        String name="kjh";
+
+        System.out.println(name);
     }
 }
